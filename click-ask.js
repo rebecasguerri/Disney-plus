85138548cad734e9e7139cadf355a7a676642151
rebecasguerri <rebecasguerri@gmail.com>
2022-08-23 12:15:25 -0300

    update

--- click-ask.js
+++ click-ask.js
@@ -1,10 +1,47 @@
-let btnAsk = document.querySelector(".ask-box");
-let btnAnswr = document.querySelector(".box-resp");
-let plus = document.querySelector(".plus")
+let btnAsk1 = document.querySelector(".ask1");
+let btnAnswr1 = document.querySelector(".resp1");
+let plus1 = document.querySelector(".plus")
 
 
-btnAsk.addEventListener("click", () =>{
-    btnAnswr.classList.toggle("active")
-    plus.classList.toggle("active")
+btnAsk1.addEventListener("click", () =>{
+    btnAnswr1.classList.toggle("active")
+    plus1.classList.toggle("active")
     console.log("rodou")
 });
+
+
+let btnAsk2 = document.querySelector(".ask2");
+let btnAnswr2 = document.querySelector(".resp2");
+let plus2 = document.querySelector(".plus2")
+
+
+btnAsk2.addEventListener("click", () =>{
+    btnAnswr2.classList.toggle("active")
+    plus2.classList.toggle("active")
+    console.log("rodou")
+});
+
+
+let btnAsk3 = document.querySelector(".ask3");
+let btnAnswr3 = document.querySelector(".resp3");
+let plus3 = document.querySelector(".plus3")
+
+
+btnAsk3.addEventListener("click", () =>{
+    btnAnswr3.classList.toggle("active")
+    plus3.classList.toggle("active")
+    console.log("rodou")
+});
+
+
+let btnAsk4 = document.querySelector(".ask4");
+let btnAnswr4 = document.querySelector(".resp4");
+let plus4 = document.querySelector(".plus4")
+
+
+btnAsk4.addEventListener("click", () =>{
+    btnAnswr4.classList.toggle("active")
+    plus4.classList.toggle("active")
+    console.log("rodou")
+});
+
